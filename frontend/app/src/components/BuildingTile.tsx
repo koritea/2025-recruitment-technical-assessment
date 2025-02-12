346bc0f5af4a057b102249d6beb5fc19f6e0739f
Yian Li <yian.li1@student.unsw.edu.au>
2025-02-12 23:14:18 +1100

    90% complete with stretch task 2

--- frontend/app/src/components/BuildingTile.tsx
+++ frontend/app/src/components/BuildingTile.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import page from "@/styles/page.module.css";
+import { useEffect, useState } from "react";
 
 const BuildingTile = ({
   name,
@@ -9,14 +12,26 @@ const BuildingTile = ({
   code: string;
   rooms: number;
 }) => {
+  const [width, setWidth] = useState(1500);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div
       style={{ backgroundImage: `url('/assets/${code}.webp')` }}
       className={page.tile}
     >
+      <div className={page.overlay}></div>
       <div className={page["rooms-available"]}>
         <div className={page.available} />
-        <p>{rooms} rooms available</p>
+        <p>
+          {rooms} {width > 650 ? "rooms available" : ` / ${rooms}`}
+        </p>
       </div>
       <div className={page["building-name"]}>{name}</div>
     </div>
